cb494d8e0f80478d92bb6ffe14daab55314c7139
Emil Gashimov <gashimow19998@gmail.com>
2022-11-15 23:12:04 +0400

    changing configs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/img/[name][ext]',
+    assetModuleFilename: 'assets/[name][ext]',
     clean: true,
   },
   devServer: {
